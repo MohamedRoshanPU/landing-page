599a48c355c926ad3f47c263aa2a5d32721f6805
MohamedRoshanPU <roshanparambil7@gmail.com>
2022-07-27 18:05:19 +0530

    commit-5

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 import Home from "./Components/Home";
-import {Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Features from "./Components/Features";
 import Works from "./Components/Works";
 import Team from "./Components/Team";
@@ -10,14 +10,16 @@ import Team from "./Components/Team";
 function App() {
   return (
     <div className="App">
+      <Router>
         <Header />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route  path="/landing-page" element={<Home />} />
           <Route path="/features" element={<Features />} />
           <Route path="/projects" element={<Works />} />
           <Route path="/team" element={<Team />} />
         </Routes>
         <Footer />
+      </Router>
     </div>
   );
 }
